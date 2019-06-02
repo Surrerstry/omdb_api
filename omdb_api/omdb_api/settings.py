1afdcc3335f4e656a3e78f4e249b2599340ef26a
Surrerstry <surrerstry@gmail.com>
2019-06-02 20:34:06 +0200

    Turn on static file, and set it to apps directory

--- omdb_api/omdb_api/settings.py
+++ omdb_api/omdb_api/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'rest_framework',
     'api_app'
 ]
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = f'{BASE_DIR}/static_root/'
